f26beadbee85d02ce6f7300dab22a5860728d041
Bruce Hammon <bhammon@gmail.com>
2016-03-13 14:09:39 -0700

    Reference bhammon fork and fixed broken link to NVM

--- setup.sh
+++ setup.sh
@@ -6,12 +6,18 @@
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+# This URL no longer works
+# curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+# Old version of node
+# nvm install v0.10.12
+# nvm use v0.10.12
+# Installs the latest version of node
+nvm install node
+nvm use node
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -23,6 +29,7 @@ sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
+# At link above cassou says that he is no longer suppoting these packages anymore
 sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
@@ -39,7 +46,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/bhammon/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
